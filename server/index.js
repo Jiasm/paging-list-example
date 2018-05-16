927c5ba4156ed2f02a91eb8568e87c4b85f80071
jiashunming <jiashunming@outlook.com>
2018-05-16 22:09:34 +0800

    update: add delete request

--- server/index.js
+++ server/index.js
@@ -22,7 +22,27 @@ router.get('/data', (ctx, next) => {
 
   ctx.body = {
     code: 200,
-    data: data.slice((pn - 1) * size, pn * size)
+    data: sliceItems(filterItems(data), (pn - 1) * size, pn * size)
+  }
+})
+
+router.delete('/data', (ctx, next) => {
+  let { items } = ctx.query
+
+  if (!items) {
+    return (ctx.body = {
+      code: 200
+    })
+  }
+
+  if (!Array.isArray(items)) {
+    items = items.split(',')
+  }
+
+  removeItems(items)
+
+  ctx.body = {
+    code: 200
   }
 })
 
@@ -31,3 +51,19 @@ app.use(router.routes()).use(router.allowedMethods())
 app.listen(12306, _ => {
   console.log(`server run as: http://127.0.0.1:12306`)
 })
+
+function sliceItems(items, start, end) {
+  return items.slice(start, end)
+}
+
+function filterItems(items) {
+  return items.filter(_ => _)
+}
+
+function removeItems(items) {
+  if (Array.isArray(items)) {
+    items.forEach(removeItems)
+  } else {
+    data[items - 1] = null
+  }
+}
